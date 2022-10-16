8e470225ea88699003aa14e551a7cfa919720428
Ubuntu <ubuntu@ip-172-31-16-168.us-west-1.compute.internal>
2022-10-16 06:55:45 +0000

    Color wheel is working

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let colorIndicator = document.getElementById('color-indicator');
-let test = document.getElementById('color-hex');
+let test = document.getElementById('userColor');
 const colorPicker = new iro.ColorPicker("#color-picker", {
   width: 180, color: "#ffffff"
 });
 colorPicker.on('color:change', function(color){
   let hexCode = color.hexString;
+  hexCode = hexCode.slice(1);
   colorIndicator.style.backgroundColor = hexCode;
-  test.innerHTML = hexCode;
+  test.value = hexCode;
 });
